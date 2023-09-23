c4bdfa52c538ffb6237f83d0166201571e52ef9e
Saoirse O'Donovan <saoirseodonovan13@gmail.com>
2023-09-23 22:04:24 +0100

    Form data adding to database and added validation
    I added the form data and made it so that the users input is added to
    the database, just like the user credentials are when signing up. I also
    added some validation so that the username provided by the user before
    filling out the quiz must exist and match the logged in user.
    
    Signed-off-by: Saoirse O'Donovan <saoirseodonovan13@gmail.com>

--- website/models.py
+++ website/models.py
@@ -7,5 +7,15 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(150), unique=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(120), nullable=False)
-    first_name = db.Column(db.String(150))
+    first_name = db.Column(db.String(150), nullable=False)
     pronouns = db.Column(db.String(100))
+
+class Quiz(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), nullable=False)
+    qusOne = db.Column(db.String(15), nullable=False)
+    qusTwo = db.Column(db.String(15), nullable=False)
+    qusThree = db.Column(db.String(15), nullable=False)
+    qusFour = db.Column(db.String(15), nullable=False)
+    qusFive = db.Column(db.String(15), nullable=False)
+    qusSix = db.Column(db.String(15), nullable=False)
